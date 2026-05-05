39f37a6343c85d2447fe7aaedac877ca980b6755
peuplade48 <saburhane92@gmail.com>
2026-05-05 14:33:55 +0300

    Add MyDeviceAdminReceiver class for device admin

---
+++ app/src/main/java/com/mdm/MyDeviceAdminReceiver.kt
@@ -0,0 +1,13 @@
+package com.mdm.smssender48
+
+import android.app.admin.DeviceAdminReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class MyDeviceAdminReceiver : DeviceAdminReceiver() {
+    override fun onEnabled(context: Context, intent: Intent) {
+        super.onEnabled(context, intent)
+        Toast.makeText(context, "MDM Yetkisi Aktif", Toast.LENGTH_SHORT).show()
+    }
+}
